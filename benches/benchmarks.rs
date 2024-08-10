8d61b49cc9e25a54af1bbbe0e703807d558e1d7e
Paul Flynn <paul@arkavo.com>
2024-08-09 20:49:30 -0400

    Switch to Criterion for benchmarks
    Replaced the nightly toolchain and Bencher library with the Criterion library for more robust benchmarking. Updated the GitHub Actions workflow to reflect this change and consolidated benchmark configuration.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,11 +1,7 @@
-#![feature(test)]
-
-extern crate test;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use nanotdf::BinaryParser;
-use test::{black_box, Bencher};
 
-#[bench]
-fn bench_read_kas_field(b: &mut Bencher) {
+fn bench_read_kas_field(c: &mut Criterion) {
     let kas_field = vec![
         0x01, 0x0e, 0x6b, 0x61, 0x73, 0x2e, 0x76, 0x69, 0x72, 0x74, 0x72, 0x75, 0x2e, 0x63, 0x6f,
         0x6d,
@@ -15,9 +11,11 @@ fn bench_read_kas_field(b: &mut Bencher) {
     let mut parser = BinaryParser::new(&kas_field);
     match parser.read_kas_field() {
         Ok(_) => {
-            b.iter(|| {
-                let mut parser = BinaryParser::new(black_box(&kas_field));
-                let _ = parser.read_kas_field().unwrap();
+            c.bench_function("read_kas_field", |b| {
+                b.iter(|| {
+                    let mut parser = BinaryParser::new(black_box(&kas_field));
+                    let _ = parser.read_kas_field().unwrap();
+                })
             });
         }
         Err(e) => {
@@ -25,3 +23,6 @@ fn bench_read_kas_field(b: &mut Bencher) {
         }
     }
 }
+
+criterion_group!(benches, bench_read_kas_field);
+criterion_main!(benches);
